8a0a7e3ed8c58be0dafa34cbeea1ada80366fa6d
Catherine J <cjarocki@gmail.com>
2017-02-20 08:47:06 -0600

    Still erroring

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,42 +4,59 @@ var currentPersonIndex = 0;
 $(document).ready(function(){
   // Upon page load, get the data from the server
   // goGetMeSomeData(); //???????
-  $('.buttonsNextPrevious').on('click', '.next', goNext);
-  $('.buttonsNextPrevious').on('click', '.previous', goPrevious);
+    console.log('jquery running');
 }); //closes document.ready
 
-function getSomeData(){
+// function getSomeData(){
   $.ajax({
     type: 'GET',
     url: '/data',
     success: function(data){
-      // console.log('returned data from server: ', data);
+      console.log('returned data from server: ', data);
       //data is inside this fxn
       phirephitersArray = data.phirephiters; //
-      // newPeopleArray(phirephitersArray);
-      // console.log("this is not working");
-      phiCarusol();
-      phiButtons(); //new functions??
-      // updateMyCarousel();
+      newPeopleArray(phirephitersArray);
+      console.log("this is working");
     } //closes success function
   }); //ajax
+// }//closes getSomeData
+
+
+//empty array to store people, now can write the rest of logic
+//last thing is to plug in
+//build all fxns, abstract them
+var personArray = [];  //have container to toss shit from fxns
+// second workspace makes abstracting more doable
+
+//build function to take in objects from JSON data and push into new empty array
+function newPeopleArray(array){
+  for (var i = 0; i < array.length; i++) {
+    personArray.push(array[i]);
+  }
 }
 
-function phiCarousel(){
+function phiCarousel(person){
   for (var i = 0; i < phirephitersArray.length; i++) {
     var $element = $('.container').children().last();
     $element.append('<p>' + 'name: ' + phirephitersArray.name + '</p>');
     $element.append('<p>' + 'git username: ' + phirephitersArray.git_username + '<p>');
     $element.append('<p>' + 'shoutout: ' + phirephitersArray.shoutout + '</p>');
-    // console.log(phiCarousel());
+    console.log(phiCarousel());
 
     // $element.hide();
   }
 }
-$('#buttonsNextPrevious').append('<button id="previous"></button>');
-// $('#buttonsNextPrevious').append('<div class="person"></div>');
-$('#buttonsNextPrevious').append('<button id="next"></button>');
 
+phiCarousel();
+phiButtons(); //new functions??
+// updateMyCarousel();
+
+$('.buttonsNextPrevious').append('<button id="previous"></button>');
+// $('#buttonsNextPrevious').append('<div class="person"></div>');
+$('.buttonsNextPrevious').append('<button id="next"></button>');
+
+$('.buttonsNextPrevious').on('click', '.next', goNext);
+$('.buttonsNextPrevious').on('click', '.previous', goPrevious);
 
 function goNext(){
   if(currentPersonIndex < phirephitersArray.length-1){
